0171e1cd3c140e09066c547c8c04845e5bba7b83
juanhapes <juanhapes@gmail.com>
2019-08-01 16:47:56 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { EventParser } = require('./lib');
+
+module.exports = EventParser;
---
+++ tests/event-parser-test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const assert = require('assert');
+
+const { EventParser, EventParserError } = require('./../lib');
+
+describe('EventParser', () => {
+
+	// your tests here...
+
+});
