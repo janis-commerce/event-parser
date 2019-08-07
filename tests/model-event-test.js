c79343512b436fdbdeed38b076180df6ae18ac11
Diego Londono <diego.angel@fizzmod.com>
2019-08-07 10:27:15 -0300

    add static to the get table & finish model testing

---
+++ tests/model-event-test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const assert = require('assert');
+const Model = require('@janiscommerce/model');
+const Settings = require('@janiscommerce/settings');
+
+const sandbox = require('sinon').createSandbox();
+
+const { ModelEvent } = require('./../lib');
+
+const modelEvent = new ModelEvent();
+
+describe('EventParser', () => {
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	it('should return empty setting when not are declared', () => {
+		assert.deepStrictEqual(ModelEvent.settings, {});
+	});
+
+	it('should return setting declared', () => {
+		const settings = { databaseKey: 'testing' };
+		sandbox.stub(Settings, 'get').returns(settings);
+
+		assert.deepStrictEqual(ModelEvent.settings, settings);
+	});
+
+	it('should return databaseKey as _default when settings are not declared', () => {
+		assert.deepStrictEqual(modelEvent.databaseKey, '_default');
+	});
+
+	it('should return databaseKey declared on settings', () => {
+		const settings = { databaseKey: 'testing' };
+		sandbox.stub(Settings, 'get').returns(settings);
+
+		assert.strictEqual(modelEvent.databaseKey, 'testing');
+	});
+
+	it('should return table as events when settings are not declared', () => {
+		assert.deepStrictEqual(ModelEvent.table, 'events');
+	});
+
+	it('should return table declared on settings', () => {
+		const settings = { table: 'testing' };
+		sandbox.stub(Settings, 'get').returns(settings);
+
+		assert.strictEqual(ModelEvent.table, 'testing');
+	});
+
+	it('should return undefined when does not receive entity or event', () => {
+		assert.deepStrictEqual(modelEvent.getAction(), undefined);
+	});
+
+	it('should return the event return by model package', () => {
+		const getEvent = {
+			client: 'core',
+			entity: 'test',
+			event: 'testing',
+			actions: [
+				{
+					source: 'shipping',
+					client: 'core/client',
+					entity: 'order',
+					event: 'create'
+				}
+			]
+		};
+		sandbox
+			.stub(Model.prototype, 'get')
+			.withArgs({
+				filters: {
+					client: 'core',
+					entity: 'test',
+					event: 'testing'
+				},
+				limit: 1
+			})
+			.returns(getEvent);
+
+		assert.deepStrictEqual(modelEvent.getAction('core', 'test', 'testing'), getEvent);
+	});
+});
