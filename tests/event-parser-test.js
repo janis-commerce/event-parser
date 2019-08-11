8f48499e99a1ed88f72ad45ac216ea9f3affa809
Diego Londono <diego.angel@fizzmod.com>
2019-08-11 09:05:10 -0300

    uppercase S in all should in tests

--- tests/event-parser-test.js
+++ tests/event-parser-test.js
@@ -26,46 +26,46 @@ describe('EventParser', () => {
 		sandbox.restore();
 	});
 
-	it('should return and instance of event parser', () => {
+	it('Should return and instance of event parser', () => {
 		assert.deepStrictEqual(Package, EventParser);
 	});
 
-	it('should throw error when not receive message', async () => {
+	it('Should throw error when not receive message', async () => {
 		await assert.rejects(EventParser.getSubscribers(), {
 			name: 'EventParserError',
 			message: 'Message is required and must be an object'
 		});
 	});
 
-	it('should throw error when message is an array', async () => {
+	it('Should throw error when message is an array', async () => {
 		await assert.rejects(EventParser.getSubscribers([]), {
 			name: 'EventParserError',
 			message: 'Message is required and must be an object'
 		});
 	});
 
-	it('should throw error when not receive entity', async () => {
+	it('Should throw error when not receive entity', async () => {
 		await assert.rejects(EventParser.getSubscribers({}), {
 			name: 'EventParserError',
 			message: 'Message entity and event are required'
 		});
 	});
 
-	it('should throw error when not receive event', async () => {
+	it('Should throw error when not receive event', async () => {
 		await assert.rejects(EventParser.getSubscribers({ entity: 'test' }), {
 			name: 'EventParserError',
 			message: 'Message entity and event are required'
 		});
 	});
 
-	it('should return empty array when event is an empty object', async () => {
+	it('Should return empty array when event is an empty object', async () => {
 		sandbox.stub(ModelEvent.prototype, 'get').returns({});
 
 		const subscribers = await EventParser.getSubscribers(message);
 		assert.deepStrictEqual(subscribers, []);
 	});
 
-	it('should return empty array when not found subscribers', async () => {
+	it('Should return empty array when not found subscribers', async () => {
 		const event = { ...getEvent };
 		event.subscribers = [];
 		sandbox.stub(ModelEvent.prototype, 'get').returns([event]);
@@ -74,7 +74,7 @@ describe('EventParser', () => {
 		assert.deepStrictEqual(subscribers, []);
 	});
 
-	it('should return the subscribers', async () => {
+	it('Should return the subscribers', async () => {
 		sandbox.stub(ModelEvent.prototype, 'get').returns([getEvent]);
 
 		const subscribers = await EventParser.getSubscribers(message);
--- tests/model-event-test.js
+++ tests/model-event-test.js
@@ -33,47 +33,47 @@ describe('EventParser', () => {
 		sandbox.restore();
 	});
 
-	it('should return empty setting when not are declared', () => {
+	it('Should return empty setting when not are declared', () => {
 		assert.deepStrictEqual(ModelEvent.settings, {});
 	});
 
-	it('should return setting declared', () => {
+	it('Should return setting declared', () => {
 		const settings = { databaseKey: 'testing' };
 		sandbox.stub(Settings, 'get').returns(settings);
 
 		assert.deepStrictEqual(ModelEvent.settings, settings);
 	});
 
-	it('should return databaseKey as _default when settings are not declared', () => {
+	it('Should return databaseKey as _default when settings are not declared', () => {
 		assert.deepStrictEqual(modelEvent.databaseKey, '_default');
 	});
 
-	it('should return databaseKey declared on settings', () => {
+	it('Should return databaseKey declared on settings', () => {
 		const settings = { databaseKey: 'testing' };
 		sandbox.stub(Settings, 'get').returns(settings);
 
 		assert.strictEqual(modelEvent.databaseKey, 'testing');
 	});
 
-	it('should return table as events when settings are not declared', () => {
+	it('Should return table as events when settings are not declared', () => {
 		assert.deepStrictEqual(ModelEvent.table, 'events');
 	});
 
-	it('should return table declared on settings', () => {
+	it('Should return table declared on settings', () => {
 		const settings = { table: 'testing' };
 		sandbox.stub(Settings, 'get').returns(settings);
 
 		assert.strictEqual(ModelEvent.table, 'testing');
 	});
 
-	it('should return undefined when does not receive entity or event', async () => {
+	it('Should return undefined when does not receive entity or event', async () => {
 		await assert.rejects(modelEvent.getEvent(), {
 			name: 'ModelEventError',
 			message: 'Entity and event are required and must be strings'
 		});
 	});
 
-	it('should return empty object when receive empty response from the model package', async () => {
+	it('Should return empty object when receive empty response from the model package', async () => {
 		sandbox
 			.stub(Model.prototype, 'get')
 			.withArgs(sampleQuery)
@@ -83,7 +83,7 @@ describe('EventParser', () => {
 		assert.deepStrictEqual(event, {});
 	});
 
-	it('should return empty object when receive empty array from model package', async () => {
+	it('Should return empty object when receive empty array from model package', async () => {
 		sandbox
 			.stub(Model.prototype, 'get')
 			.withArgs(sampleQuery)
@@ -93,7 +93,7 @@ describe('EventParser', () => {
 		assert.deepStrictEqual(event, {});
 	});
 
-	it('should return the event return by model package', async () => {
+	it('Should return the event return by model package', async () => {
 		sandbox
 			.stub(Model.prototype, 'get')
 			.withArgs(sampleQuery)
